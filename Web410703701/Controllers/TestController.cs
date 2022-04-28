8dc7bea8169c92714efafd61798ae2708bd3dbea
xujiaxiang <xujiaxiang@gmail.com>
2022-04-28 20:52:26 +0800

    0428

---
+++ Web410703701/Controllers/TestController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Web410703701.Controllers
+{
+    public class TestController : Controller
+    {
+        // GET: Default
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult HW3()
+        {
+            return View();
+        }
+    }
+}
---
+++ Web410703701/Controllers/TodayController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Web410703701.Controllers
+{
+    public class TodayController : Controller
+    {
+        // GET: Today
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
