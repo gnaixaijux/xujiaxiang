3730afd94d06f25ea151a2431df937adc2469d3c
xujiaxiang <xujiaxiang@gmail.com>
2022-05-05 20:45:34 +0800

    0505

---
+++ Web410703701/Controllers/textController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Web410703701.Controllers
+{
+    public class textController : Controller
+    {
+        // GET: text
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
